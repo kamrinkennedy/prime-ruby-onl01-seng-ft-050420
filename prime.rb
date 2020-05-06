7c6a6fae626ddafa72b84111cceaabf7715ecb35
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 21:10:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,13 @@ def prime?(integer)
   if integer <= 1 
     return false
   end
+  # Loop through every integer between '2' and given integer
   i = 2 
   while i < integer
+  # If given integer is evenly divisible by any number between it and '2', it is not prime
     return false if integer % i == 0
     i += 1
   end
+  # Otherwise, the number has to prime
   true
 end

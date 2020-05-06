d97964a6fd795cca0e683af948e8da86173a7cd8
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 20:50:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,8 @@
-# Add  code here!
+# Method to determine wether or not an integer is a prime number
 def prime?(integer)
+  # There are no prime numbers lower than the number '2'
+  if integer < 2 
+    false 
+  elsif 
   
 end
